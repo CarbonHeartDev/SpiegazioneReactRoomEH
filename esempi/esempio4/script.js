243153694a4fc583080818eae6f63fc894411068
Emiliano Sandri <emisandri@outlook.it>
2021-09-24 09:56:08 +0200

    Aggiunta struttura base per esempio 4

--- esempi/esempio4/script.js
+++ esempi/esempio4/script.js
@@ -2,7 +2,11 @@
 const App = () => {
     return (
         <div id="app-container">
+            <div className="upper-part">
+                <span className="red-button">Cancella</span>
+                <span className="output-box">0</span>
+            </div>
+            <input type="text" className="input-box" />
         </div>
     );
 };
-
